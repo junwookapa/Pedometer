4211b49eb0fe346a05c88bfcedfa2d6b9e1af919
jun <junwookapa@naver.com>
2016-12-05 07:41:53 +0900

    final commit

--- app/src/main/java/com/threabba/android/pedometer/components/MainActivity.java
+++ app/src/main/java/com/threabba/android/pedometer/components/MainActivity.java
@@ -6,14 +6,18 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.content.ServiceConnection;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.os.PersistableBundle;
 import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
@@ -46,12 +50,11 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 
-
 /**
  * Created by jun on 16. 12. 3.
  */
 
-public class MainActivity extends AppCompatActivity implements PedometerFragment.CallBack{
+public class MainActivity extends AppCompatActivity implements PedometerFragment.CallBack {
 
 
     // tab layout ref : https://github.com/Swalloow/AndroidStudy
@@ -62,9 +65,9 @@ public class MainActivity extends AppCompatActivity implements PedometerFragment
 
     private DaoSession mDaoSession;
     private App mApp;
-    private static final String RECORD="RECORD";
+    private static final String RECORD = "RECORD";
     private Record mRecord;
-    private static final String ACTIVE="ACTIVE";
+    private static final String ACTIVE = "ACTIVE";
     private boolean mIsActiveStep;
     // 서비스
     private GPSService mGPSService;
@@ -74,21 +77,23 @@ public class MainActivity extends AppCompatActivity implements PedometerFragment
     private TabPagerAdapter pagerAdapter;
     private Toast mToast;
     private final DecimalFormat mDecimalFormat = new DecimalFormat("0.##");
+
+
     /**
      * 1. 생명주기 관리
-     * */
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-        mDaoSession = ((App)getApplication()).getDaoSession();
+        mDaoSession = ((App) getApplication()).getDaoSession();
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             Window window = getWindow();
             window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
             window.setStatusBarColor(Color.BLACK);
         }
-        mApp = (App)getApplication();
+        mApp = (App) getApplication();
 
         // Initializing the TabLayout
         tabLayout.addTab(tabLayout.newTab().setText("만보기 화면"));
@@ -139,43 +144,28 @@ public class MainActivity extends AppCompatActivity implements PedometerFragment
         mRecord = DBManager.getRecord(mDaoSession);
         mToast = Toast.makeText(this, "", Toast.LENGTH_SHORT);
 
-
-        if(mApp.isLocationNetworkEanble() || mApp.isLocationGPSEnable()){
-            bindService(new Intent(MainActivity.this, GPSService.class), gpsConnection, Context.BIND_AUTO_CREATE);
-        }else{
-            showGPSAlert();
-        }
         Intent intent = new Intent(this, StepService.class);
         startService(intent);
         bindService(new Intent(MainActivity.this, StepService.class), stepConnection, Context.BIND_AUTO_CREATE);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
     }
 
     PermissionListener permissionlistener = new PermissionListener() {
         @Override
         public void onPermissionGranted() {
+            if(!bindGpsService()){
+                showGPSAlert();
+            }
         }
+
         @Override
         public void onPermissionDenied(ArrayList<String> deniedPermissions) {
-
+            if(!bindGpsService()){
+                showGPSAlert();
+            }
         }
     };
-
-    private void showGPSAlert() {
-            // GPS OFF 일때 Dialog 표시
-            AlertDialog.Builder gsDialog = new AlertDialog.Builder(this);
-            gsDialog.setTitle("위치 서비스 설정");
-            gsDialog.setMessage("무선 네트워크 사용, GPS 위성 사용을 모두 체크하셔야 정확한 위치 서비스가 가능합니다.\n위치 서비스 기능을 설정하시겠습니까?");
-            gsDialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                public void onClick(DialogInterface dialog, int which) {
-                    // GPS설정 화면으로 이동
-                    Intent intent = new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                    intent.addCategory(Intent.CATEGORY_DEFAULT);
-                    startActivityForResult(intent, Const.GPS_ARELT);
-                }
-            }).create().show();
-
-    }
-
 
 
     @Override
@@ -187,11 +177,9 @@ public class MainActivity extends AppCompatActivity implements PedometerFragment
     @Override
     public void startActivityForResult(Intent intent, int requestCode, Bundle options) {
         super.startActivityForResult(intent, requestCode, options);
-        switch(requestCode){
+        switch (requestCode) {
             case Const.GPS_ARELT:
-                if(mApp.isLocationNetworkEanble() || mApp.isLocationGPSEnable()){
-                    bindService(new Intent(MainActivity.this, GPSService.class), gpsConnection, Context.BIND_AUTO_CREATE);
-                }
+                bindGpsService();
                 break;
         }
     }
@@ -200,7 +188,7 @@ public class MainActivity extends AppCompatActivity implements PedometerFragment
     @Override
     protected void onResume() {
         super.onResume();
-        if(isStepServiceBound && mStepService.isOverrayActive()){
+        if (isStepServiceBound && mStepService.isOverrayActive()) {
             mStepService.finishOverayView();
         }
     }
@@ -208,14 +196,14 @@ public class MainActivity extends AppCompatActivity implements PedometerFragment
     @Override
     protected void onPause() {
         super.onPause();
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
-            if(Settings.canDrawOverlays(this)){
-                if(isStepServiceBound && !mStepService.isOverrayActive() && mIsActiveStep){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (Settings.canDrawOverlays(this)) {
+                if (isStepServiceBound && !mStepService.isOverrayActive() && mIsActiveStep) {
                     mStepService.initOverayView(mRecord);
                 }
             }
-        }else{
-            if(isStepServiceBound && !mStepService.isOverrayActive() && mIsActiveStep){
+        } else {
+            if (isStepServiceBound && !mStepService.isOverrayActive() && mIsActiveStep) {
                 mStepService.initOverayView(mRecord);
             }
         }
@@ -223,20 +211,18 @@ public class MainActivity extends AppCompatActivity implements PedometerFragment
 
     @Override
     protected void onStop() {
-        super.onStop();
+        super.onStop();// ATTENTION: This was auto-generated to implement the App Indexing API.
         mToast.cancel();
-
-
 
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        if(isGPSServiceBound){
+        if (isGPSServiceBound) {
             unbindService(gpsConnection);
         }
-        if(isStepServiceBound){
+        if (isStepServiceBound) {
             unbindService(stepConnection);
         }
     }
@@ -244,7 +230,7 @@ public class MainActivity extends AppCompatActivity implements PedometerFragment
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
-        if(savedInstanceState != null){
+        if (savedInstanceState != null) {
             mRecord = savedInstanceState.getParcelable(RECORD);
             mIsActiveStep = savedInstanceState.getBoolean(ACTIVE);
         }
@@ -267,7 +253,7 @@ public class MainActivity extends AppCompatActivity implements PedometerFragment
     @Override
     public void onRestoreInstanceState(Bundle savedInstanceState, PersistableBundle persistentState) {
         super.onRestoreInstanceState(savedInstanceState, persistentState);
-        if(savedInstanceState != null){
+        if (savedInstanceState != null) {
             mRecord = savedInstanceState.getParcelable(RECORD);
             mIsActiveStep = savedInstanceState.getBoolean(ACTIVE);
         }
@@ -275,12 +261,21 @@ public class MainActivity extends AppCompatActivity implements PedometerFragment
 
     /**
      * 2. 서비스 관리
-     * **/
+     **/
+    private boolean bindGpsService(){
+        if (mApp.isLocationNetworkEanble() || mApp.isLocationGPSEnable()) {
+            bindService(new Intent(MainActivity.this, GPSService.class), gpsConnection, Context.BIND_AUTO_CREATE);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     ServiceConnection gpsConnection = new ServiceConnection() { //gps 커넥션
         @Override
         public void onServiceConnected(ComponentName name, IBinder service) {
-            mGPSService = ((GPSService.GPSBinder)service).getService();
-            if(mGPSService != null){
+            mGPSService = ((GPSService.GPSBinder) service).getService();
+            if (mGPSService != null) {
                 isGPSServiceBound = true;
                 mGPSService.registerCallBack(callBack);
             }
@@ -296,9 +291,9 @@ public class MainActivity extends AppCompatActivity implements PedometerFragment
     GPSService.CallBack callBack = new GPSService.CallBack() { // GPS 서비스 콜백
         @Override
         public void onUpdateAddress(String address, int msg) {
-            switch (msg){
+            switch (msg) {
                 case GPSService.MESSAGE_SUCCESS:
-                    ((PedometerFragment)pagerAdapter.instantiateItem(viewPager, 0)).setAddress(address);
+                    ((PedometerFragment) pagerAdapter.instantiateItem(viewPager, 0)).setAddress(address);
                     break;
                 case GPSService.MESSAGE_FAIL:
                     break;
@@ -317,8 +312,8 @@ public class MainActivity extends AppCompatActivity implements PedometerFragment
     ServiceConnection stepConnection = new ServiceConnection() { //스텝 커넥션
         @Override
         public void onServiceConnected(ComponentName name, IBinder service) {
-            mStepService = ((StepService.StepServiceBinder)service).getService();
-            if(mStepService != null){
+            mStepService = ((StepService.StepServiceBinder) service).getService();
+            if (mStepService != null) {
                 isStepServiceBound = true;
                 mStepService.registeCallBack(stepListener);
             }
@@ -333,7 +328,7 @@ public class MainActivity extends AppCompatActivity implements PedometerFragment
     StepListener stepListener = new StepListener() {
         @Override
         public void onStep() { // 스텝 발생 콜백
-            if(mRecord != null && mIsActiveStep) {
+            if (mRecord != null && mIsActiveStep) {
                 int stepCnt = mRecord.getStep_count() + 1;
                 float distance = stepCnt * Const.STEP_PER_KM;
                 mRecord.setStep_count(stepCnt);
@@ -347,10 +342,11 @@ public class MainActivity extends AppCompatActivity implements PedometerFragment
                 }
 
                 ((RecordFragment) pagerAdapter.instantiateItem(viewPager, 1)).notifyDataSetChanged(mDaoSession);
-            }else{
+            } else {
 
             }
         }
+
         @Override
         public void passValue() {
 
@@ -366,6 +362,20 @@ public class MainActivity extends AppCompatActivity implements PedometerFragment
     /**
      * 4. 기타
      * **/
+    private void showGPSAlert() {
+        // GPS OFF 일때 Dialog 표시
+        AlertDialog.Builder gsDialog = new AlertDialog.Builder(this);
+        gsDialog.setTitle("위치 서비스 설정");
+        gsDialog.setMessage("무선 네트워크 사용, GPS 위성 사용을 모두 체크하셔야 정확한 위치 서비스가 가능합니다.\n위치 서비스 기능을 설정하시겠습니까?");
+        gsDialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                // GPS설정 화면으로 이동
+                Intent intent = new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                intent.addCategory(Intent.CATEGORY_DEFAULT);
+                startActivityForResult(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS), Const.GPS_ARELT);
+            }
+        }).create().show();
 
+    }
 
 }
